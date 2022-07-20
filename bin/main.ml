3ffedc7fef1bb5651b719612895ca083fd0f2ccb
Jinho <yoonjinho0318@gmail.com>
2022-07-20 11:43:21 -0400

    feat: test yojson with example route

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,19 @@
 open Gentzen
 open Typing
+open Yojson
 
 let () =
   Dream.run @@ Dream.logger
   @@ Dream.router
        [
          Dream.get "/" (fun _ -> Dream.html "Good morning, world!");
+         Dream.post "/"
+          (fun request ->
+            let%lwt body = Dream.body request in
+            let message_object = body |> Yojson.Safe.from_string in
+            let _ = Format.printf "Parsed to %a" Yojson.Safe.pp message_object in
+            Dream.html "Something!"
+          );
          Dream.get "/api/test/:name" (fun request ->
              Dream.html (Dream.param request "name"));
        ]
