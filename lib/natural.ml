1a2c662ffb86bfb26d11b85b1b59f0ccb90784a2
Jinho Yoon <yoonjinho0318@gmail.com>
2023-01-08 20:02:26 -0500

    feat: add natural deduction module

--- lib/natural.ml
+++ lib/natural.ml
@@ -8,45 +8,45 @@ type nd_proof =
   | Assum of expr
   | Proof of { cons: expr; assums: nd_proof list; rule: rule }
 
-let join chosen left right = failwith "err"
+(* let join chosen left right = failwith "err" *)
 
-let rec convert tree top bottom =
-  match tree with
-  | Proof { applied = (prop, rule); subproofs } ->
-      (* if right rule, then *)
-        (* find applied prop in the leaves of the bottom trees *)
-        (* then replace leaf with applied prop *)
+(* (* let rec convert tree top bottom = *) *)
+  (* (* match tree with *) *)
+  (* (* | Proof { applied = (prop, rule); subproofs } -> *) *)
+      (* (* if right rule, then *) *)
+        (* (* find applied prop in the leaves of the bottom trees *) *)
+        (* (* then replace leaf with applied prop *) *)
       
-      (* else, if left rule, then *)
-        (* find applied prop in the root of the top trees *)
-        (* then add the rule *)
+      (* (* else, if left rule, then *) *)
+        (* (* find applied prop in the root of the top trees *) *)
+        (* (* then add the rule *) *)
 
-      (* traverse down the subproofs *)
-        (* One -> *)
+      (* (* traverse down the subproofs *) *)
+        (* (* One -> *) *)
 
-        (* Two -> *)
-          (* And_R ->  *)
-      failwith "err"
-  | Axiom (left, right) ->
-      let chosen = find_finished left right in
-      join chosen left right
+        (* (* Two -> *) *)
+          (* (* And_R ->  *) *)
+      (* failwith "err" *)
+  (* | Axiom (left, right) -> *)
+      (* let chosen = find_finished left right in *)
+      (* join chosen left right *)
 
-let convert tree : nd_proof =
-  let rec convert (tree : Sequent.proof) assums conseqs =
-    match tree with
-    | Proof { applied = (prop, rule); subproofs } ->
-        let (assums', conseqs') =
-          match rule with
-          (* | And_R -> List.find (fun x -> x = prop) conseqs *)
-          | _ -> failwith "err"
-        in
-        begin
-          match subproofs with
-          | One sub -> convert sub assums conseqs
-          | _ -> failwith "err"
-        end
-    | Axiom (left, right) ->
-        let chosen = List.find (fun x -> List.mem x right) left in
-        join chosen left right
-  in
-  convert tree [] []
+(* let convert tree : nd_proof = *)
+  (* let rec convert (tree : Sequent.proof) assums conseqs = *)
+    (* match tree with *)
+    (* | Proof { applied = (prop, rule); subproofs } -> *)
+        (* let (assums', conseqs') = *)
+          (* match rule with *)
+          (* (* | And_R -> List.find (fun x -> x = prop) conseqs *) *)
+          (* | _ -> failwith "err" *)
+        (* in *)
+        (* begin *)
+          (* match subproofs with *)
+          (* | One sub -> convert sub assums conseqs *)
+          (* | _ -> failwith "err" *)
+        (* end *)
+    (* | Axiom (left, right) -> *)
+        (* let chosen = List.find (fun x -> List.mem x right) left in *)
+        (* join chosen left right *)
+  (* in *)
+  (* convert tree [] [] *)
